137de6da0c12fff99e57f8aa048f97b200920ce5
Kevin Mongiello <kmong92@gmail.com>
2016-08-30 18:51:55 -0400

    Add Game Options

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 
 Flappy Bird is a browser game built in pure Javascript and uses HTML Canvas for animation.  The objective is simple: Fly between the pipes for as long as you can.  You can use the keys 'w' and 'up arrow' to fly.  When the game ends, simply follow the instructions and press 'enter' to play again.  Check your high-score in the top-left corner.
 
+### User modifications
+
+If you think the game is too hard, (or too easy), clone the repo and change some of the settings to fit your fancy!  Included is a `game_constants.js` file where you'll find easy-to-tune parameters to control things like the spawn rate of the pipes, speed of the bird, and gravity!(...and more).  To play the new version
+
 ### Technical
 
 The top-level game file extracts the canvas DOM element and passes it's 2D-context down to a GameView to be further passed down to the game and it's components in order to render themselves.  The GameView binds keys to events to make jumping possible and declares window intervals so that animations can be rendered.  The animation interval is set to 33ms in order to display animations at 30fps.  
--- dist/bundle.js
+++ dist/bundle.js
@@ -45,6 +45,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	const GameView = __webpack_require__(1);
+	const Constants = __webpack_require__(4);
 
 	function draw() {
 	  const canvas = document.getElementById("flappy-bird");
@@ -52,7 +53,8 @@
 	  if (!localStorage.highScore) {
 	    localStorage.highScore = "0";
 	  }
-	  const gv = new GameView(canvasCtx);
+	  Constants.hardness(document.getElementById('game-level').value);
+	  const gv = new GameView(canvasCtx, Constants);
 
 	}
 
@@ -60,17 +62,21 @@
 	  draw();
 	});
 
+	window.gConstants = __webpack_require__(4);
+
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
 	const Game = __webpack_require__(2);
+	// const Constants = require('./game_constants');
 
-	const GameView = function(ctx) {
-	  this.game = new Game(this);
+	const GameView = function(ctx, Constants) {
+	  this.Constants = Constants;
+	  this.game = new Game(this, this.Constants);
 	  this.ctx = ctx;
-	  this.bird = this.game.addBird();
+	  this.bird = this.game.addBird(this.Constants);
 	  this.game.addPipes();
 	  this.bindKeyHandlers();
 	  this.jumpCount = this.game.jumpCount;
@@ -82,9 +88,8 @@
 	GameView.prototype.keypress = function(e) {
 	  if (e.key === "ArrowUp" || e.key === "w") {
 	    if (this.jumpCount < 1) {
-	      this.game.foreground[0].vel = [-1, 0];
-	      this.bird.accel = [0, 0.28];
-	      //  switch on pipes
+	      this.game.foreground[0].vel = this.Constants.GAME_VELOCITY;
+	      this.bird.accel = this.Constants.GRAVITY;
 	      this.game.gameSwitch = true;
 	      this.bird.jump();
 	      this.jumpCount++;
@@ -103,15 +108,17 @@
 	  document.removeEventListener("keydown", this.keypress);
 	};
 
-	GameView.prototype.animateCallBack = function () {
-
+	GameView.prototype.animateCallBack = function (time) {
 	  if (this.gameOver === true) {
 	    this.stop();
 	    return;
 	  }
+
+	  let timeDelta = time - this.lastTime;
 	  this.gameFrame ++;
-	  this.game.step(this.gameFrame);
+	  this.game.step(this.gameFrame, timeDelta);
 	  this.game.draw(this.ctx);
+	  this.lastTime = time;
 	  requestAnimationFrame(this.animateCallBack.bind(this));
 	};
 
@@ -124,6 +131,7 @@
 	  this.gameOver = false;
 	  this.bird.pos = [100, 100];
 	  this.gameFrame = 0;
+	  this.lastTime = 0;
 	  this.startInterval();
 	};
 
@@ -132,7 +140,9 @@
 	  this.removeHandlers();
 	  let resetHandler = (e) => {
 	    if (e.keyCode === 13) {
-	      new GameView(this.ctx);
+	      let hardness = document.getElementById('game-level').value;
+	      this.Constants.hardness(hardness);
+	      new GameView(this.ctx, this.Constants);
 	      window.removeEventListener('keydown', resetHandler);
 	    }
 	  };
@@ -148,16 +158,18 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	const Bird = __webpack_require__(3);
-	const Pipe = __webpack_require__(6);
-	const Constants = __webpack_require__(7);
+	const Pipe = __webpack_require__(7);
+	// const Constants = require('./game_constants');
 	const Foreground = __webpack_require__(8);
+	const Util = __webpack_require__(6);
 
-	const Game = function(gv) {
+	const Game = function(gv, Constants) {
 	  this.gv = gv;
+	  this.Constants = Constants;
 	  this.score = 0;
 	  this.pipes = [];
-	  this.DIM_X = Constants.WIDTH;
-	  this.DIM_Y = Constants.HEIGHT;
+	  this.DIM_X = this.Constants.WIDTH;
+	  this.DIM_Y = this.Constants.HEIGHT;
 	  this.background = document.getElementById('background');
 	  this.foreground = [new Foreground(this, { pos: [0, 410], vel: [0, 0] } )];
 	  this.gameOver = false;
@@ -165,6 +177,7 @@
 	  this.gameSwitch = false;
 	  this.jumpCount = 0;
 	  this.addPipes();
+	  this.pipeSpawnRate = Util.clampNum(this.Constants.PIPE_SPAWN_RATE, 100, 150);
 	};
 
 	Game.prototype.draw = function(ctx) {
@@ -182,6 +195,7 @@
 	  ctx.fillStyle = "white";
 	  if (!this.gameSwitch) {
 	    ctx.font = '20px serif';
+	    ctx.fillText("To Start:", 50, 20);
 	    ctx.fillText("Press 'w' or 'up' arrow to jump!", 50, 40);
 	  } else{
 	    ctx.font = '48px serif';
@@ -206,38 +220,38 @@
 	  return this.pipes.concat(this.bird).concat(this.foreground);
 	};
 
-	Game.prototype.moveObjects = function() {
+	Game.prototype.moveObjects = function(timeDelta) {
 	  this.allObjects().forEach(function (object) {
-	    object.move();
+	    object.move(timeDelta);
 	  });
 	};
 
 	Game.prototype.checkForeground = function() {
-	  if (this.foreground[0].pos[0] + Constants.WIDTH < 0) {
+	  if (this.foreground[0].pos[0] + this.Constants.WIDTH < 0) {
 	    this.foreground.shift();
 	  }
 	  // debugger
 	  if (this.foreground[0].pos[0] < 0 && this.foreground.length < 2) {
-	    this.foreground.push(new Foreground(this, { pos: [350, 410], vel: [-1, 0] }));
+	    this.foreground.push(new Foreground(this, { pos: [350, 410], vel: this.Constants.GAME_VELOCITY }));
 	  }
 	};
 
 
-	Game.prototype.step = function(gameFrame) {
-	  this.moveObjects();
+	Game.prototype.step = function(gameFrame, timeDelta) {
+	  this.moveObjects(timeDelta);
 	  this.isCollision();
 	  this.checkForeground();
 	  if (this.isOutOfBounds(this.bird)) {
 	    this.stopGame();
 	  }
-	  if (gameFrame % 100 === 0 && !this.gameOver) { this.addPipes();}
+	  if (gameFrame % this.pipeSpawnRate === 0 && !this.gameOver) { this.addPipes();}
 	};
 
 	Game.prototype.addBird = function() {
 	  const bird = new Bird({
 	    pos: [110, 20],
 	    game: this
-	  });
+	  }, this.Constants);
 	  this.bird = bird;
 
 	  return bird;
@@ -256,7 +270,7 @@
 	    game: this,
 	    pos: [this.DIM_X + 10, 0],
 	    gapNum: _randNum()
-	  });
+	  }, this.Constants);
 
 	  pipes.forEach((pipe) => this.pipes.push(pipe) );
 	  this.removePipes();
@@ -265,7 +279,7 @@
 	Game.prototype.isOutOfBounds = function(obj) {
 	  let x = obj.pos[0], y = obj.pos[1];
 
-	  if (x < 0 || y > this.GROUND_Y - 20) {
+	  if (x < 0 || y > this.GROUND_Y - 15) {
 	    return true;
 	  }
 	  return false;
@@ -323,11 +337,13 @@
 /* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
-	const MovingObject = __webpack_require__(4);
-	const Util = __webpack_require__(5);
+	// const Constants = require('./game_constants');
+	const MovingObject = __webpack_require__(5);
+	const Util = __webpack_require__(6);
 
-	const Bird = function(obj) {
+	const Bird = function(obj, Constants) {
 	  MovingObject.call(this, {game: obj.game, pos: obj.pos, vel: [0, 0], radius: 15, color: "green"});
+	  this.Constants = Constants;
 	  this.accel = [0, 0];
 	  // FINAL ACCEL = [0, 0.28];
 	  this.image = document.getElementById('bird');
@@ -376,31 +392,33 @@
 	  );
 	};
 
-	Number.prototype.clamp = function(min, max) {
-	  return Math.min(Math.max(this, min), max);
+
+	Bird.prototype._getVel = function(vel) {
+	  return Util.clampNum(vel, -this.Constants.BIRD_MAX_VEL, this.Constants.BIRD_MAX_VEL);
 	};
 
-	function _getVel(vel) {
-	  return vel.clamp(-8, 8);
-	}
-
-	Bird.prototype.move = function () {
+	Bird.prototype.move = function (timeDelta) {
+	  let moveRatio = this.Constants.NORMAL_TIME_DELTA / timeDelta;
+	  moveRatio = 1;
 	  this.birdFrame ++;
 	  if (this.birdFrame % this.flapSpeed === 0) {
 	    this.sx = (this.sx + 92) % 276;
 	  }
-	  this.vel[1] = _getVel(this.vel[1]);
-	  this.vel[1] += this.accel[1];
-	  if (this.vel[1] <= -1) {
+	  this.vel[1] = this._getVel(this.vel[1]);
+	  this.vel[1] += (this.accel[1]);
 
+	  if (this.vel[1] <= -1) {
 	    this.flapSpeed = 4;
 	  } else if (this.vel[0] === 0){
 	    this.flapSpeed = 17;
 	  } else {
 	    this.flapSpeed = 7;
 	  }
-	  this.pos[0] += this.vel[0];
-	  this.pos[1] += this.vel[1];
+
+	  if (moveRatio === moveRatio) {
+	    this.pos[0] += (this.vel[0] * moveRatio);
+	    this.pos[1] += (this.vel[1] * moveRatio);
+	  }
 	};
 
 	module.exports = Bird;
@@ -408,7 +426,49 @@
 
 /***/ },
 /* 4 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
+
+	const Util = __webpack_require__(6);
+
+	module.exports = {
+	  //// Leave These Alone...
+	  WIDTH: 350,
+	  HEIGHT: 525,
+	  NORMAL_TIME_DELTA: 1000/60,
+
+	  //// ...And Play With These :)
+	  GRAVITY: [0.1, 0.28],
+	  GAME_VELOCITY: [-1, 0],
+	  BIRD_MAX_VEL: 8,
+	  // Choose EXTRA_PIPE_WIDTH between 0 - 30
+	  EXTRA_PIPE_WIDTH: 0,
+	  // Choose PIPE_SPAWN_RATE between 100 - 150
+	  PIPE_SPAWN_RATE: 100,
+	  hardness: function(num) {
+
+	    let hardness = Util.clampNum(parseInt(num), 0, 3);
+	    if (hardness === 0) {
+	      this.EXTRA_PIPE_WIDTH = 30;
+	      this.PIPE_SPAWN_RATE = 150;
+	    } else if (hardness === 1) {
+	      this.EXTRA_PIPE_WIDTH = 25;
+	      this.PIPE_SPAWN_RATE = 130;
+	    } else if (hardness === 2) {
+	      this.EXTRA_PIPE_WIDTH = 10;
+	      this.PIPE_SPAWN_RATE = 115;
+	    } else if (hardness === 3) {
+	      this.EXTRA_PIPE_WIDTH = 0;
+	      this.PIPE_SPAWN_RATE = 100;
+	    }
+	  },
+	};
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const Constants = __webpack_require__(4);
 
 	const MovingObject = function(options) {
 	  this.pos = options.pos;
@@ -418,9 +478,14 @@
 	  this.game = options.game;
 	};
 
-	MovingObject.prototype.move = function () {
-	  this.pos[0] += this.vel[0];
-	  this.pos[1] += this.vel[1];
+
+	MovingObject.prototype.move = function (timeDelta) {
+	  let moveRatio = Constants.NORMAL_TIME_DELTA / timeDelta;
+	  moveRatio = 1;
+	  if (moveRatio === moveRatio) {
+	    this.pos[0] += (this.vel[0] * moveRatio);
+	    this.pos[1] += (this.vel[1] * moveRatio);
+	  }
 	};
 
 
@@ -428,7 +493,7 @@
 
 
 /***/ },
-/* 5 */
+/* 6 */
 /***/ function(module, exports) {
 
 	module.exports = {
@@ -437,20 +502,23 @@
 	    Surrogate.prototype = parentClass.prototype;
 	    childClass.prototype = new Surrogate();
 	    childClass.prototype.constructor = childClass;
+	  },
+	  clampNum (num, min, max) {
+	    return Math.min(Math.max(num, min), max);
 	  }
 	};
 
 
 /***/ },
-/* 6 */
+/* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
-	const GameConstants = __webpack_require__(7);
-	const MovingObject = __webpack_require__(4);
-	const Util = __webpack_require__(5);
+	// const Constants = require('./game_constants');
+	const MovingObject = __webpack_require__(5);
+	const Util = __webpack_require__(6);
 
 	const Pipe = function(options) {
-	  vel = [-1, 0];
+	  vel = options.vel;
 	  pos = options.pos;
 	  MovingObject.call(this, {game: options.game, vel: vel, pos: pos, color: "green"} );
 	  this.w = 20;
@@ -463,8 +531,9 @@
 	};
 	Util.inherits(Pipe, MovingObject);
 
-	Pipe.prototype.addPipes = function(options) {
+	Pipe.prototype.addPipes = function(options, Constants) {
 	  // Top Pipe
+	  options.vel = Constants.GAME_VELOCITY;
 	  options.h = 30 * options.gapNum;
 	  options.image = document.getElementById("pipe_180");
 	  options.image_origin = (options.h - 50);
@@ -473,8 +542,10 @@
 	  let pipe1 = new Pipe(options);
 
 	  // Bottom Pipe
-	  let pos1 = options.pos[0];
-	  options.pos = [pos1, (30 * options.gapNum) + 90];
+	  let posX = options.pos[0];
+	  let pipeWidth = 90 + Util.clampNum(Constants.EXTRA_PIPE_WIDTH, 0, 30);
+	  options.pos = [posX, (30 * options.gapNum) + pipeWidth];
+
 	  options.h = 600;
 	  options.image = document.getElementById("pipe");
 	  options.pipe_bottom = document.getElementById("pipe_bottom");
@@ -484,6 +555,10 @@
 
 	  return [pipe1, pipe2];
 	};
+
+	// function clampWidth(ExtraWidth) {
+	//   return Math.min(Math.max(ExtraWidth, 0), 30);
+	// }
 
 
 	Pipe.prototype.draw = function (ctx) {
@@ -499,21 +574,11 @@
 
 
 /***/ },
-/* 7 */
-/***/ function(module, exports) {
-
-	module.exports = {
-	  WIDTH: 350,
-	  HEIGHT: 525
-	};
-
-
-/***/ },
 /* 8 */
 /***/ function(module, exports, __webpack_require__) {
 
-	const MovingObject = __webpack_require__(4);
-	const Util = __webpack_require__(5);
+	const MovingObject = __webpack_require__(5);
+	const Util = __webpack_require__(6);
 
 	const Foreground = function(game, options) {
 	  this.image = document.getElementById('ground_chopped');
